24fe302ed9fb004b57a2c7e1c295cf748dd5a4b5
Dhruv Sahney <37184867+dsahney@users.noreply.github.com>
2018-04-16 11:03:49 -0400

    Update test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -1,11 +1,12 @@
 import unittest
+import reverse
 import algorithm1
 import algorithm2
 import algorithm3
 
-# Test cases for algorithm 1
-class algorithm1_test(unittest.TestCase):
-    ''' Checks the functions in algorithm1, to ensure they are working as desired. '''
+# Test cases for function reverse
+class revese_test(unittest.TestCase):
+    ''' Checks the reverse function, to ensure it works as desired. '''
     def test_reverse_ex1(self):
         ''' Ensures function reverse, reverses a string. '''
         actual = algorithm1.reverse('apple')
@@ -17,7 +18,10 @@ class algorithm1_test(unittest.TestCase):
         actual = algorithm1.reverse('strts')
         expected = 'strts'
         self.assertEqual(actual, expected)
-        
+
+# Test cases for algorithm 1
+class algorithm1_test(unittest.TestCase):
+    ''' Checks the functions in algorithm1, to ensure they are working as desired. '''
     def test_pal_check_1_ex1(self):
         ''' Ensures function pal_check_1, reports whether a string is a palindrome or not. '''
         actual = algorithm1.pal_check_1('strts')
