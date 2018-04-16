37f2d3bf07bcbf2220825fb5e3068b5983f36170
Dhruv Sahney <37184867+dsahney@users.noreply.github.com>
2018-04-16 11:00:45 -0400

    Update algorithm1.py

--- algorithm1.py
+++ algorithm1.py
@@ -1,16 +1,4 @@
-# Below is a function that takes a string, s, and reverses its characters.
-def reverse(s):
-    ''' (str) -> str
-    Return a string that has been reversed.
-    >>> reverse('apple')
-    'elppa'
-    >>> reverse('strts')
-    'strts'
-    '''
-    new_string = '' # this will act as an accumulator.
-    for character in s:
-        new_string = character + new_string # we add the new characters from iterations before the previous string, thus, reversing the string.
-    return new_string
+import reverse
 
 # Below is a function that compares the original string to the reversed string to determine whether it is a palindrome.
 def pal_check_1(s):
