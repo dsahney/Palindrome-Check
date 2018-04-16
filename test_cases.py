526402ec09283e6c49f268fce88c96901e38b4b4
Dhruv Sahney <37184867+dsahney@users.noreply.github.com>
2018-04-16 10:57:47 -0400

    Update test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -1,10 +1,11 @@
 import unittest
 import algorithm1
-# import algorithm2
-# import algorithm3
+import algorithm2
+import algorithm3
 
+# Test cases for algorithm 1
 class algorithm1_test(unittest.TestCase):
-    ''' Checks the functions in algorithm1 to ensure they are working as desired. '''
+    ''' Checks the functions in algorithm1, to ensure they are working as desired. '''
     def test_reverse_ex1(self):
         ''' Ensures function reverse, reverses a string. '''
         actual = algorithm1.reverse('apple')
@@ -29,5 +30,35 @@ class algorithm1_test(unittest.TestCase):
         expected = False
         self.assertEqual(actual, expected)
         
+# Test cases for algorithm 2
+class algorithm2_test(unittest.TestCase):
+    ''' Checks the functions in algorithm2, to ensure they are working as desired. '''
+    def test_pal_check_2_ex1(self):
+        ''' Ensures function pal_check_2, reports whether a string is a palindrome or not. '''
+        actual = algorithm2.pal_check_2('asdsa')
+        expected = True
+        self.assertEqual(actual, expected)
+        
+    def test_pal_check_2_ex2(self):
+        ''' Ensures function pal_check_2, reports whether a string is a palindrome or not. '''
+        actual = algorithm2.pal_check_2('asd')
+        expected = False
+        self.assertEqual(actual, expected)
+        
+# Test cases for algorithm 3
+class algorithm3_test(unittest.TestCase):
+    ''' Checks the functions in algorithm3, to ensure they are working as desired. '''
+    def test_pal_check_3_ex1(self):
+        ''' Ensures function pal_check_3, reports whether a string is a palindrome or not. '''
+        actual = algorithm3.pal_check_3('strts')
+        expected = True
+        self.assertEqual(actual, expected)
+        
+    def test_pal_check_3_ex2(self):
+        ''' Ensures function pal_check_3, reports whether a string is a palindrome or not. '''
+        actual = algorithm3.pal_check_3('asd')
+        expected = False
+        self.assertEqual(actual, expected)
+    
 if _name_ == '_main_':
     unittest.main(exit=False)
